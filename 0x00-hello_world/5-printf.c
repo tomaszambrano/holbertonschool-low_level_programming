6938abbf3bfa9d34ce10f55b2d5254b7ea1cca88
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-23 15:20:41 -0700

    Working with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a text with printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,%c\n");
+return (0);
+}

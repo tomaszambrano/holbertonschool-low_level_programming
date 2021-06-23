9b53c2c0f1c758afdc80d7590a6363ea14149e9a
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-23 15:50:39 -0700

    Apply Betty Style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-/**
- * Author Tomas Zambrano, Code Reused found it on internet
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 printf("Size of a char: %i byte(s)\n", sizeof(char));
@@ -12,5 +6,5 @@ printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
-  return (0);
+return (0);
 }

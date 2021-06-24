3cabf0411d8ea59410fe9eec1ff5c9f5c0043e9d
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-24 15:25:44 -0700

    Add

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,18 +8,18 @@
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-  if(n>0){
-    printf("positive");
-  }
-  else if(n==0){
-    printf("is zero");
-  }
-  else{
-    printf("is negative");
-  }
-  return (0);
+if(n > 0)
+printf ("positive");
+
+else if(n == 0)
+printf ("is zero");
+
+else
+printf ("is negative");
+
+return (0);
 }

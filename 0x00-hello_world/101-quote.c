3f04dc8d4713cc9dd734518aef652c27e3787233
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-23 21:26:17 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-**
-* main - Entry point - Program to print strings
-*
-* Return: Always 0 (Success)
-*
-int main(void) {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return 0;
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+  return (1);
 }

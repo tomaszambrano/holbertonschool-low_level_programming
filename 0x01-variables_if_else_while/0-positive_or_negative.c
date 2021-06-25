bd54cd1c95a5ed9f9b66d7c6b1b8a9689fa9aed4
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-24 17:06:29 -0700

    Updating

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Randon Num
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 
-if(n > 0)
-printf ("positive");
+  int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
-else if(n == 0)
-printf ("is zero");
+  if(n > 0)
+    printf("%d positive\n", n);
 
-else
-printf ("is negative");
+  else if(n == 0)
+    printf("%d is zero\n", n);
 
-return (0);
+  else
+    printf("%d is negative \n", n);
+
+  return (0);
 }

eaf83f9e059eb102796bc6378d66af28fcc3c280
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-24 18:18:04 -0700

    updating code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,16 +13,17 @@ int n, lastdigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastdigit = n % 10;
-if ( lastdigit > 5)
+if (lastdigit > 5)
 {
-printf("Last digit of: %d is %d and is greater than 5\n", n, lastdigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
-else if (lastdigit == 0) {
-printf("Last digit of: %d is %d and is greater than 0\n", n, lastdigit);
+else if (lastdigit == 0)
+{
+printf("Last digit of %d is %d and is greater than 0\n", n, lastdigit);
 }
 else if (lastdigit < 6 && lastdigit != 0)
 {
-printf("Last digit of: %d is %d and is less than 6\n", n, lastdigit);
+printf("Last digit of %d is %d and is less than 6\n", n, lastdigit);
 }
 return (0);
 }

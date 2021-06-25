2fcab70c786f01db6ce42d4ec5686947d630b384
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-24 19:37:52 -0700

    Alphabet lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,15 @@
 int main(void)
 {
 char ch;
+char ch2;
 for (ch = 'a'; ch <= 'z'; ch++)
+{
 putchar(ch);
-
+}
+for (ch2 = 'A'; ch2 <= 'Z'; ch2++)
+{
+putchar(ch2);
+}
 putchar('\n');
 return (0);
 }

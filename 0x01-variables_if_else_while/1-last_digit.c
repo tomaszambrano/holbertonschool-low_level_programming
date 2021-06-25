245ea0f83581ff2ea6cfe179997b535808039ddf
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-24 17:40:34 -0700

    Lasti digit of a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the last dgit of a number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit: %d\n", n%10);
+return (0);
+}

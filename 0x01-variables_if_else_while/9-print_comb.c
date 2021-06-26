e694fe7b41a117d58764e3c1c82692267480a197
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-25 21:06:32 -0700

    Holberton

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,17 @@ int main(void)
 int num;
 for (num = '0'; num <= '9'; num++)
 {
+if(num == '9')
+}
+putchar(num);
+}
+else
+{
 putchar(num);
 putchar(',');
 putchar(' ');
 }
+}
 putchar('\n');
 return (0);
 }
-
---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Holbertons
+ *
+ * Return: Always (Success)
+ */
+
+int main ()
+{
+printf("Holberton\n");
+return 0;
+}
+ 

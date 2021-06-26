a0e9420bf1e890cfaabbc9ac7c44ed6762fe0707
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-25 19:41:41 -0700

    Update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a hexadecimal string
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char hex;
+for (hex = '0'; hex <= '9'; hex++)
+{
+putchar(hex);
+}
+for (hex = 'a'; hex <= 'f'; hex++)
+{
+putchar(hex);
+}
+putchar('\n');
+return (0);
+}

7165033003b80cc9c8414a255765d216ef7fe41a
tomaszambrano <gerencia@simbiotica.com.co>
2021-06-25 17:30:20 -0700

    Updating

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,17 +7,13 @@
  */
 int main(void)
 {
-  char ch;
-  for (ch = 'a'; ch <= 'z'; ch++)
-    {
-      if(ch == 'e'  || ch == 'q'){
-
-      }else{
-	putchar(ch);
-      }
-
-    }
-
-  putchar('\n');
-  return (0);
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if(ch != 'e'  && ch != 'q'){
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
 }
